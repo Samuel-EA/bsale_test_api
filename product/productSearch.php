9341666121f864962051fe842ded04a52e914531
Samuel EA <samuel.ea19@outlook.com>
2022-04-20 19:52:56 -0500

    REQUEST_METHOD ADDED

--- product/productSearch.php
+++ product/productSearch.php
@@ -5,7 +5,12 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Auth-Key");
- 
+
+if($_SERVER["REQUEST_METHOD"] == "OPTIONS"){
+    // set response code - 200 OK
+    http_response_code(200);
+}
+
 // import database connection
 require_once '../config/database.php';
  
