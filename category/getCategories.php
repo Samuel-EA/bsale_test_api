d033ee31fb4c0bfb68c8b734b30c81f6c134d08d
Samuel EA <samuel.ea19@outlook.com>
2022-04-20 20:25:44 -0500

    preflight

--- category/getCategories.php
+++ category/getCategories.php
@@ -9,57 +9,59 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 if($_SERVER["REQUEST_METHOD"] == "OPTIONS"){
     // set response code - 200 OK
     http_response_code(200);
-}
-
-
-// import database connection
-require_once '../config/database.php';
- 
-// import openpay object
-require_once '../objects/category.php';
-
-// import AUTH_KEY object
-require_once '../resources/OAuthSimulation.php';
- 
-//instantiate database connection
-$database = new DatabaseBsale();
-$db = $database->getConnection();
-
-//get AUTH_KEY
-if(isset($_SERVER['HTTP_AUTH_KEY'])){
-    $headerAuthKey = $_SERVER['HTTP_AUTH_KEY'];
+    return 0;
 }else{
-    $headerAuthKey = "";
-}
+    
 
-//instantiate OAuthSimulation
-$OAuthSimulation = new OAuthSimulation($headerAuthKey);
+    // import database connection
+    require_once '../config/database.php';
+    
+    // import openpay object
+    require_once '../objects/category.php';
 
-//make sure request is authorized
-if(!$OAuthSimulation->validateAuthKey()){
-    // set response code - 403 forbbiden
-    http_response_code(403);
- 
-    // tell the user
-    echo json_encode(array("error" => "Unauthorized request"));
-}else{
-    $getCategoryAttempt = new Category($db);
- 
-    // try to get cageory list
-    if($result = $getCategoryAttempt->getCategories()){
-        // set response code - 200 OK
-        http_response_code(200);
-        // tell the user
-        echo $result; 
+    // import AUTH_KEY object
+    require_once '../resources/OAuthSimulation.php';
+    
+    //instantiate database connection
+    $database = new DatabaseBsale();
+    $db = $database->getConnection();
+
+    //get AUTH_KEY
+    if(isset($_SERVER['HTTP_AUTH_KEY'])){
+        $headerAuthKey = $_SERVER['HTTP_AUTH_KEY'];
+    }else{
+        $headerAuthKey = "";
     }
-    
-    // if unable to get category list, tell the user
-    else{
-        // set response code - 503 service unavailable
-        http_response_code(503);
+
+    //instantiate OAuthSimulation
+    $OAuthSimulation = new OAuthSimulation($headerAuthKey);
+
+    //make sure request is authorized
+    if(!$OAuthSimulation->validateAuthKey()){
+        // set response code - 403 forbbiden
+        http_response_code(403);
     
         // tell the user
-        echo json_encode(array("message" => "Unable to get category data."));
+        echo json_encode(array("error" => "Unauthorized request"));
+    }else{
+        $getCategoryAttempt = new Category($db);
+    
+        // try to get cageory list
+        if($result = $getCategoryAttempt->getCategories()){
+            // set response code - 200 OK
+            http_response_code(200);
+            // tell the user
+            echo $result; 
+        }
+        
+        // if unable to get category list, tell the user
+        else{
+            // set response code - 503 service unavailable
+            http_response_code(503);
+        
+            // tell the user
+            echo json_encode(array("message" => "Unable to get category data."));
+        }
     }
 }
 ?>
--- product/getProductsByCategory.php
+++ product/getProductsByCategory.php
@@ -9,80 +9,82 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 if($_SERVER["REQUEST_METHOD"] == "OPTIONS"){
     // set response code - 200 OK
     http_response_code(200);
-}
-
-
-// import database connection
-require_once '../config/database.php';
- 
-// import productsByCategory object
-require_once '../objects/productsByCategory.php';
-
-// import AUTH_KEY object
-require_once '../resources/OAuthSimulation.php';
-
-//instantiate database connection
-$database = new DatabaseBsale();
-$db = $database->getConnection();
-
-//get AUTH_KEY
-if(isset($_SERVER['HTTP_AUTH_KEY'])){
-    $headerAuthKey = $_SERVER['HTTP_AUTH_KEY'];
-}else{
-    $headerAuthKey = "";
-}
-
-//instantiate OAuthSimulation
-$OAuthSimulation = new OAuthSimulation($headerAuthKey);
-
-//make sure request is authorized
-if(!$OAuthSimulation->validateAuthKey()){
-    // set response code - 403 forbbiden
-    http_response_code(403);
- 
-    // tell the user
-    echo json_encode(array("error" => "Unauthorized request"));
-} else {
-    // get posted data
-    $data = json_decode(file_get_contents("php://input"));
-    // $data = file_get_contents("php://input");
-
-    // make sure data is not empty
-    if(
-        !empty($data) &&
-        $data != null
-    ){
-        $getProductsByCategoryAttempt = new Product($db,"product",$data->records,$data->page,$data->category);
-        
-        $result = $getProductsByCategoryAttempt->getProductsByCategory();
-
-        // return response
-        if(!isset($result["error"])){
-                // set response code - 200 OK
-                http_response_code(200);
+    return 0;
+}else {
     
-                // tell the user
-                echo json_encode($result);
-        }
-        // if unable to get response, tell the user
-        else{
+
+    // import database connection
+    require_once '../config/database.php';
     
-            // set response code - 503 service unavailable
-            http_response_code(503);
-    
-            // tell the user
-            echo json_encode($result);
-        }
+    // import productsByCategory object
+    require_once '../objects/productsByCategory.php';
+
+    // import AUTH_KEY object
+    require_once '../resources/OAuthSimulation.php';
+
+    //instantiate database connection
+    $database = new DatabaseBsale();
+    $db = $database->getConnection();
+
+    //get AUTH_KEY
+    if(isset($_SERVER['HTTP_AUTH_KEY'])){
+        $headerAuthKey = $_SERVER['HTTP_AUTH_KEY'];
+    }else{
+        $headerAuthKey = "";
     }
-    
-    // tell the user data is incomplete
-    else{
-    
-        // set response code - 400 bad request
-        http_response_code(400);
+
+    //instantiate OAuthSimulation
+    $OAuthSimulation = new OAuthSimulation($headerAuthKey);
+
+    //make sure request is authorized
+    if(!$OAuthSimulation->validateAuthKey()){
+        // set response code - 403 forbbiden
+        http_response_code(403);
     
         // tell the user
-        echo json_encode(array("message" => "Unable to retrieve required request data."));
+        echo json_encode(array("error" => "Unauthorized request"));
+    } else {
+        // get posted data
+        $data = json_decode(file_get_contents("php://input"));
+        // $data = file_get_contents("php://input");
+
+        // make sure data is not empty
+        if(
+            !empty($data) &&
+            $data != null
+        ){
+            $getProductsByCategoryAttempt = new Product($db,"product",$data->records,$data->page,$data->category);
+            
+            $result = $getProductsByCategoryAttempt->getProductsByCategory();
+
+            // return response
+            if(!isset($result["error"])){
+                    // set response code - 200 OK
+                    http_response_code(200);
+        
+                    // tell the user
+                    echo json_encode($result);
+            }
+            // if unable to get response, tell the user
+            else{
+        
+                // set response code - 503 service unavailable
+                http_response_code(503);
+        
+                // tell the user
+                echo json_encode($result);
+            }
+        }
+        
+        // tell the user data is incomplete
+        else{
+        
+            // set response code - 400 bad request
+            http_response_code(400);
+        
+            // tell the user
+            echo json_encode(array("message" => "Unable to retrieve required request data."));
+        }
     }
 }
 ?>
